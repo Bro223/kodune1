a7dffbddf923d00acb3b1ac9986924c61ba0f1f6
Bro223 <Aleks.bratsu@gmail.com>
2023-02-18 23:16:38 +0200

    ready header

---
+++ head.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+#include <string.h>
+#define MAX_NAME_LEN 20
+#define MAX_HOBBIES_LEN 10
+#define MAX_HOBBY_LEN 20
+
+typedef struct {
+char name[MAX_NAME_LEN];
+int H;
+char hobbies[MAX_HOBBIES_LEN][MAX_HOBBY_LEN];
+} Person;
+
+void getPersons(Person persons[], int *n);
+void getHobbies(Person persons[], int n, char hobby[], Person fans[], int *m);
+void writeFans(Person fans[], int m);
+
+#endif
