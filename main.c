ddae1b16076e5f002578895e4b74fc4e6358deea
Bro223 <Aleks.bratsu@gmail.com>
2023-02-19 02:47:36 +0200

    updated main

--- main.c
+++ main.c
@@ -1,74 +1,33 @@
+/**
+ * File: main.c
+ * Author: Aleks Bratsun 222821IACB
+ * Created: 07.02.2023
+ * Last edit: 19.02.2023
+ * Kirjeldus: 1 kodune ylesanne
+ */
+ 
+/* INCLUDID */
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include "string.h"
+#include "head.h"
+#include "funkt.c"
 
-#define MAX_NAME_LENGTH 30
-#define MAX_HOBBIES 10
-#define MAX_HOBBY_LENGTH 30
-
-struct record {
-  char name[MAX_NAME_LENGTH];
-  int h;
-  char hobbies[MAX_HOBBIES][MAX_HOBBY_LENGTH];
-};
-
-int read_records_from_file(char *filename, struct record records[], int max_records) {
-  FILE *fp;
-  fp = fopen(filename, "r");
-  if (fp == NULL) {
-    printf("Error opening file %s\n", filename);
-    return 0;
-  }
-
-  int i, j;
-  for (i = 0; i < max_records; i++) {
-    if (fscanf(fp, "%s %d", records[i].name, &records[i].h) == EOF) {
-      break;
-    }
-    for (j = 0; j < records[i].h; j++) {
-      fscanf(fp, "%s", records[i].hobbies[j]);
-    }
-  }
-
-  fclose(fp);
-  return i;
-}
-
-void print_record(struct record r) {
-  int i;
-  printf("%s %d\n", r.name, r.h);
-  for (i = 0; i < r.h; i++) {
-    printf("%s\n", r.hobbies[i]);
-  }
-}
-
-void find_and_print_records_by_hobby(char *hobby, struct record records[], int num_records) {
-  int i, j;
-  for (i = 0; i < num_records; i++) {
-    for (j = 0; j < records[i].h; j++) {
-      if (strcmp(hobby, records[i].hobbies[j]) == 0) {
-        printf("%s\n", records[i].name);
-        break;
-      }
-    }
-  }
-}
+#include "head.h"
 
 int main() {
-  char filename[100];
-  printf("Enter the name of the file: ");
-  scanf("%s", filename);
+    Person persons[100];
+    int n;
+    char hobby[MAX_HOBBY_LEN];
+    Person fans[100];
+    int m;
 
-  struct record records[100];
-  int num_records = read_records_from_file(filename, records, 100);
-  if (num_records == 0) {
-    return 1;
-  }
-
-  char hobby[MAX_HOBBY_LENGTH];
-  printf("Enter the name of a hobby: ");
-  scanf("%s", hobby);
-
-  find_and_print_records_by_hobby(hobby, records, num_records);
-
-  return 0;
+    getPersons(persons, &n);
+    printf("Enter hobby: ");
+    scanf("%s", hobby);
+    getHobbies(persons, n, hobby, fans, &m);
+    writeFans(fans, m);
+    
+    return 0;
+    
 }
